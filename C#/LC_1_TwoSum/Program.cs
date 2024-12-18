785b9b6b8941b029abb0659b658c09e62d2916aa
Rafael Oliva <rafa.oliva950713@hotmail.com>
2024-12-17 22:34:20 -0600

    Adding problem 1 Two Sum Leetcode

---
+++ C#/LC_1_TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = TwoSum(nums, target);
+        foreach (int i in result)
+        {
+            Console.Write(i + " ");
+        }
+    }
+    
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result.Add(i);
+                    result.Add(j);
+                }
+            }
+        }
+        return result.ToArray();
+    }
+}
