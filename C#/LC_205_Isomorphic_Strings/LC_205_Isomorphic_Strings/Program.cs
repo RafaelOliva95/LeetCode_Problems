d00db4d79c4dbd2338016a4428bf3c58d47beaaa
Rafael Oliva <rafa.oliva950713@hotmail.com>
2024-12-18 10:55:14 -0600

    Adding problem 205 Isomorphic Strings

---
+++ C#/LC_205_Isomorphic_Strings/LC_205_Isomorphic_Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.IsolatedStorage;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool result = IsIsomorphic("foo", "bar");
+        Console.WriteLine(result);
+    }
+    public static bool IsIsomorphic(string s, string t)
+    {
+        Dictionary<char, char> values = new Dictionary<char, char>();
+        if (s.Length != t.Length)
+            return false;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (values.ContainsKey(s[i]))
+            {
+                if (values[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (values.ContainsValue(t[i]))
+                {
+                    return false;
+                }
+                values.Add(s[i], t[i]);
+            }
+        }
+
+        return true;
+    }
+}
